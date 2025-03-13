e0892470054692e6c4e4c7a173f3790cf590f395
Alessio <alessiomarcelli04@gmail.com>
2025-03-13 15:22:40 +0100

    fix link PBI

--- Components/PBI/reportsConfig.js
+++ Components/PBI/reportsConfig.js
@@ -7,8 +7,8 @@ export const futures = {
             embedUrl: "https://app.powerbi.com/view?r=eyJrIjoiNzIxNDIzZTgtOGMzMC00OGUzLTlhY2UtZDJjZWY3YzFhZmI3IiwidCI6IjY5ZGExM2FmLTc4Y2ItNGRkOS1iMjBjLTA4NzU1MGYyYjkxMiIsImMiOjl9",
         },
         futuresAnalysis: {
-            reportId: "38831d35-3aac-49b8-8f1a-8afbf6f96e43",
-            embedUrl: "https://app.powerbi.com/view?r=eyJrIjoiNzIxNDIzZTgtOGMzMC00OGUzLTlhY2UtZDJjZWY3YzFhZmI3IiwidCI6IjY5ZGExM2FmLTc4Y2ItNGRkOS1iMjBjLTA4NzU1MGYyYjkxMiIsImMiOjl9",
+            reportId: "77bc07c1-bdb0-4c1f-aaf6-1eedcc2be1b6",
+            embedUrl: "https://app.powerbi.com/reportEmbed?reportId=77bc07c1-bdb0-4c1f-aaf6-1eedcc2be1b6&autoAuth=true&ctid=69da13af-78cb-4dd9-b20c-087550f2b912",
         },
         past: {
             reportId: "f9f7a1d8-32e8-4b04-9cb8-9f83c0e25858",
